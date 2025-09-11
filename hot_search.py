03f70da53c4e63da43afac933433d0fc17ee22df
duzqiu <duzqiu@outlook.com>
2025-09-11 11:24:56 +0800

    增加可以获取单个平台数据的接口

--- hot_search.py
+++ hot_search.py
@@ -11,7 +11,10 @@ mongo_server = MongoDBServer()
 @app.route('/api/hot_search/list', methods=['GET'])
 def get_hot_search(limit=10):
     platform_list = [
-        "baidu", "toutiao", "dy_hot", "dy_plant","dy_entertain","dy_society","dy_sh"
+        "baidu", "toutiao", "bilibili",
+        "dy_hot", "dy_plant", "dy_entertain", "dy_society", "dy_sh",
+        "sina_hot", "sina_cmnt", "sina_video", "sina_trend", "sina_sport", 
+        "sina_ent", "sina_auto", "sina_fashion","sina_travel", "sina_ai"
         ] # 支持的平台列表
     try: 
         result = dict()  # 初始化结果字典
@@ -26,9 +29,26 @@ def get_hot_search(limit=10):
         
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
+    
+@app.route('/api/hot_search/<platform>/<int:limit>', methods=['GET'])
+@app.route('/api/hot_search/<platform>', methods=['GET'])
+def get_hot_search_by_platform(platform, limit=100):
+    try: 
+        result = dict()  # 初始化结果字典
+        cursor = mongo_server.get_data(platform, limit=limit) # 获取指定平台的数据
+        result[platform] = list(cursor) # 将游标转换为列表
+        return jsonify({
+            "success": True, 
+            "count": len(result),
+            "data": mongo_server.serialize_doc(result) # 序列化文档
+        }), 200
+        
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
 
 if __name__ == '__main__':
     print("Flask 服务启动中...")
-    print("API 地址: http://localhost:5000/api/hot_search/list")
+    print("API 地址: http://localhost:5000/api")
     app.run(host='0.0.0.0', port=5000, debug=True)
     
